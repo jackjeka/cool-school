f631c41575d34c86172fff003722e402af2bf6f5
jack_jeka <angie.or.jackie@gmail.com>
2014-12-08 23:57:45 +0200

    added 'specialization'

---
+++ src/Santa/CoolSchoolBundle/Entity/Specialization.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Santa\CoolSchoolBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Specialization
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Santa\CoolSchoolBundle\Entity\SpecializationRepository")
+ */
+class Specialization
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="schools", type="array")
+     */
+    private $schools;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Specialization
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set schools
+     *
+     * @param array $schools
+     * @return Specialization
+     */
+    public function setSchools($schools)
+    {
+        $this->schools = $schools;
+
+        return $this;
+    }
+
+    /**
+     * Get schools
+     *
+     * @return array 
+     */
+    public function getSchools()
+    {
+        return $this->schools;
+    }
+}

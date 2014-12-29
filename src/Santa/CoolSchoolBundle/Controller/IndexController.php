0f163b5c238468e6840fe594057143535e653c6a
jackjeka <angie.or.jackie@gmail.com>
2014-12-29 03:18:56 +0200

    add language switcher

--- src/Santa/CoolSchoolBundle/Controller/IndexController.php
+++ src/Santa/CoolSchoolBundle/Controller/IndexController.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace Santa\CoolSchoolBundle\Controller;
-use Santa\CoolSchoolBundle\Entity\School;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class IndexController extends Controller
@@ -15,8 +15,10 @@ class IndexController extends Controller
      * @Route("/")
      * @Method({"GET"})
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $locale = $request->getLocale();
+
         $schools = $this->getDoctrine()->getManager()->getRepository('SantaCoolSchoolBundle:School')->findAll();
 
         return ['schools' => $schools];
